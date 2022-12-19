7a6e957e4c2ff2a3b93283f8106c12079e94166e
Pedrolo77 <pedroochoarosales@gmail.com>
2022-12-19 10:16:31 -0500

    Agregandole cositas al drawer xd

--- lib/pages/Home_Page.dart
+++ lib/pages/Home_Page.dart
@@ -18,8 +18,38 @@ class _HomePageState extends State<HomePage> {
         )
       ]),
       drawer: Drawer(
-        backgroundColor: Colors.blueAccent,
+      child: ListView(
+        // Important: Remove any padding from the ListView.
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text('Drawer Header'),
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.home,
+            ),
+            title: const Text('Page 1'),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.train,
+            ),
+            title: const Text('Page 2'),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
       ),
+    ),
     );
-  }
+  
+}
 }
